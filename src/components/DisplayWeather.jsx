dad454257620ada76f4ca4665bba1d24dd4dfdbd
MacreenSmith <macreensmith@gmail.com>
2023-08-04 09:55:28 -0400

    Adjusted submit button, added the degrees to the weather card and the weather description

--- src/components/DisplayWeather.jsx
+++ src/components/DisplayWeather.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import './displayWeather.css'
 
-function DisplayWeather (props) {
-    console.log(props)
-  return (
-    <div>
-      
-    </div>
-  )
+function DisplayWeather({data}) {
+    // console.log(props)
+    // const {data} = props;
+    return (
+        <div className="displayweather">
+            <div className="maincard">
+                <span className="cardtitle">
+                {data.name}, {data.sys.country}, Weather
+                </span>
+                <span className="cardsubtitle">
+                As of {new Date().toLocaleTimeString()}
+                </span>
+                <h1>
+                    {Math.floor((data.main.temp -273.15) * 1.8)  + 32} &#8457;
+                </h1>
+                <span className="weather-main">
+                    {data.weather[0].description}
+                </span>
+            </div>
+
+        </div>
+    )
 }
 
 export default DisplayWeather
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -15,59 +15,65 @@ function Weather() {
     //FETCHING WEATHER DATA
 
     async function weatherData(e) {
-    e.preventDefault()
+        e.preventDefault()
         //checking if the input has a value
-        if(form.city == ""){
+        if (form.city == "") {
             alert("Add Values")
         } else {
-            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&appid=${APIKEY}`) 
-            const data = await res.json()
-            setWeather({data : data})
+            try {
+                //making the api call
+                const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&appid=${APIKEY}`)
+                const data = await res.json()
+                setWeather({ data: data })
+            } catch (error) {
+                console.log(error)
+            }
+
         }
     }
-    
-    
-    
+
+
+
 
     const handleChange = (e) => {
         let name = e.target.name
         let value = e.target.value
 
-        if(name == "city"){
+        if (name == "city") {
             setForm({
                 ...form,
                 city: value
             });
         }
-        if(name == "country"){
+        if (name == "country") {
             setForm({
                 ...form,
                 country: value
             });
         }
-        
+
     };
     // console.log(form.city, form.country)
-  return (
-    <div className="weather">
-      <span className="title"> Weather App </span>
-      <br />
-      <form action="">
-        <input type="text" name="city" placeholder="city" onChange={handleChange}/>
-        &nbsp; &nbsp; &nbsp; &nbsp;
-        <input type="text" name="country" placeholder="country" onChange={handleChange}/>
-        <button className="getWeather" onClick={weatherData}>Submit</button>
-      </form>
+    return (
+        <div className="weather">
+            <span className="title"> Weather App </span>
+            <br />
+            <form action="">
+                <input type="text" name="city" placeholder="city" onChange={handleChange} />
+                &nbsp; &nbsp; &nbsp; &nbsp;
+                <input type="text" name="country" placeholder="country" onChange={handleChange} />
+                <button className="getweather" onClick={weatherData}>Submit</button>
+            </form>
 
-      {
-        weather.data !== undefined ? (
-        <div> 
-        <DisplayWeather  date={weather.data}/>
+            {
+                weather.data !== undefined ? (
+                    <div>
+                        <DisplayWeather data={weather.data} />
+                    </div>
+                ) : null
+            }
         </div>
-         ) : null
-      }
-    </div>
-  )
+    )
 }
 
 export default Weather
